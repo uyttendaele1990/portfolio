5f9a972b1bcd48f0a838e26619bc606deab34c3d
Kenneth Uyttendaele <34237704+uyttendaele1990@users.noreply.github.com>
2018-08-08 14:06:25 +0200

    afwerking responsive portfolio

---
+++ dist/dist/about.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Over Mij</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <header>
+        <div class="menu-btn">
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+        </div>
+
+        <nav class="menu">
+            <div class="menu-branding">
+                <div class="portrait"></div>
+            </div>
+            <ul class="menu-nav">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item current">
+                    <a href="about.php" class="nav-link">
+                        Over mij
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="work.php" class="nav-link">
+                        Mijn Portfolio
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.php" class="nav-link">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main id="about">
+        <h1 class="lg-heading">
+            Over
+            <span class='text-secondary'>Mij</span>
+        </h1>
+        <h2 class="sm-heading">
+            Kenneth Uyttendaele
+        </h2>
+        <div class="about-info">
+            <img src="img/iksm.png" alt="Me" class="bio-image">
+            <div class="bio">
+                <h3 class="text-secondary">Info</h3>
+                <p>Ik ben Kenneth Uyttendaele 27 jaar, omgeschoold tot web-developer in Syntra.<br>
+                Ik heb een echte hobby gevonden in programmeren,<br>
+                De mogelijkheden zijn onbegrensd de enige grens is de skill-level van de programmeur.<br>
+                Het is mijn droom om uiteindelijk een full-stack developer te worden. </p>
+            </div>    
+        </div>
+    </main>
+
+    <footer class="main-footer">
+        Copyright &copy;
+        <?php echo date("Y"); ?>
+    </footer></center>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ dist/dist/contact.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Contact</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <header>
+        <div class="menu-btn">
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+        </div>
+
+        <nav class="menu">
+            <div class="menu-branding">
+                <div class="portrait"></div>
+            </div>
+            <ul class="menu-nav">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.php" class="nav-link">
+                        Over Mij
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="work.php" class="nav-link">
+                        Mijn Portfolio
+                    </a>
+                </li>
+                <li class="nav-item current">
+                    <a href="contact.php" class="nav-link">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main id="contact">
+    <h1 class="lg-heading">
+            Contacteer
+            <span class='text-secondary'>Mij</span>
+        </h1>
+        <div class="boxes">
+            <div>
+                <span class="text-secondary">Email:</span> uyttendaele1990@gmail.com
+            </div>
+            <div>
+                <span class="text-secondary">telefoon:</span> 0496982421
+            </div>
+            <div>
+                <span class="text-secondary">Adres:</span> René Vermandelstraat 14, 9060 Zelzate
+            </div>
+        </div>
+    </main>
+
+    <footer class="main-footer">
+        Copyright &copy;
+        <?php echo date("Y"); ?>
+    </footer></center>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
